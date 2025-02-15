c86eccdc50a0c5540618dbd0d124e03a2b97a0e3
Majd <er1uxt4csb398y4k5g3w15695snm6l@bots.bitbucket.org>
2025-02-15 19:14:23 +0000

    clean up, ts, tw and shadcn

---
+++ client/tailwind.config.ts
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // Use export default for ES modules
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
