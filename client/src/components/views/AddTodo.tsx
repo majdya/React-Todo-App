bc1f725fae3a15fadb4d132787bc6b914329866f
Majd <er1uxt4csb398y4k5g3w15695snm6l@bots.bitbucket.org>
2025-02-16 11:43:40 +0000

    add limit to input ui

--- client/src/components/views/AddTodo.tsx
+++ client/src/components/views/AddTodo.tsx
@@ -5,6 +5,7 @@ import { Plus } from "lucide-react";
 import { v4 as uuidv4 } from "uuid";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Todo } from "@/types/todo";
+const MAX_LENGTH = 50;
 
 const AddTodo = () => {
   const queryClient = useQueryClient();
@@ -12,6 +13,7 @@ const AddTodo = () => {
 
   const addTodoMutation = useMutation({
     mutationFn: async (newTodo: Todo) => {
+      // TODO: replace with api
       return newTodo;
     },
     onSuccess: (newTodo) => {
@@ -25,7 +27,7 @@ const AddTodo = () => {
 
   const handleAddTodo = () => {
     const text = newTodoText.trim();
-    if (text) {
+    if (text.length > 0) {
       addTodoMutation.mutate({
         id: uuidv4(),
         title: text,
@@ -35,14 +37,22 @@ const AddTodo = () => {
   };
 
   return (
-    <div className="flex items-center p-3 gap-3">
+    <div className="flex items-center p-3 gap-3 relative">
       <Input
         placeholder="Add a New Todo"
         value={newTodoText}
         onChange={(e) => setNewTodoText(e.target.value)}
-        className="flex-grow"
+        maxLength={MAX_LENGTH}
+        className="flex-grow pr-14"
       />
-      <Button onClick={handleAddTodo}>
+      <Button
+        onClick={handleAddTodo}
+        disabled={!newTodoText.trim()}
+        className="relative"
+      >
+        <span className="absolute bottom-2.5 right-18 text-xs text-gray-600  px-1 rounded">
+          {newTodoText.length}/{MAX_LENGTH}
+        </span>
         <Plus size={40} />
       </Button>
     </div>
