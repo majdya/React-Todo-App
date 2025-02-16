136b2cf34c66a5f753fd206697fd8a3dcb42cfbe
Majd <er1uxt4csb398y4k5g3w15695snm6l@bots.bitbucket.org>
2025-02-16 11:44:28 +0000

    limit display line to 2 on sm and 1 lg, impelemnt onRemove callback

--- client/src/components/views/TodoCard.tsx
+++ client/src/components/views/TodoCard.tsx
@@ -1,28 +1,37 @@
-// import { Card, CardHeader, CardTitle } from "../ui/card";
-
 import { Checkbox } from "@/components/ui/checkbox";
 import { TodoCardProps } from "@/types/todo";
-
 import { Label } from "@/components/ui/label";
+import { Trash } from "lucide-react";
+import { Button } from "../ui/button";
 
-const TodoCard = ({ id, title, completed, onClick }: TodoCardProps) => {
+const TodoCard = ({
+  id,
+  title,
+  completed,
+  onClick,
+  onRemove,
+}: TodoCardProps) => {
   return (
-    <>
-      <div className={` p-4`}>
-        <div className={`flex items-center space-x-4  `}>
-          <Checkbox id={id} checked={completed} onCheckedChange={onClick} />
-          <Label
-            className={`
-              text-3xl 
-              ${completed ? `line-through text-indigo-400` : ""}
-              `}
-            htmlFor={id}
-          >
-            {title}
-          </Label>
-        </div>
+    <div className="px-4 py-2 m-1 rounded-lg bg-gray-100 shadow-sm">
+      <div className="flex items-center space-x-4">
+        <Button variant="destructive" onClick={() => onRemove(id)}>
+          <Trash />
+        </Button>
+        <Checkbox
+          id={id}
+          checked={completed}
+          onCheckedChange={() => onClick(id)}
+        />
+        <Label
+          className={` line-clamp-2 lg:line-clamp-1 text-2xl ${
+            completed ? "line-through text-indigo-400" : ""
+          }`}
+          htmlFor={id}
+        >
+          {title}
+        </Label>
       </div>
-    </>
+    </div>
   );
 };
 
