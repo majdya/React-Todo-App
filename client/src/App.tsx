5c1fbd1a4b29b40fa5279e149ffc3d5d5f254ddd
majdya <majd.ya.1995@gmail.com>
2025-02-17 11:26:45 +0000

    fix bg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <div className="min-h-[500px] pt-5 bg-gradient-to-tr from-zinc-200 via-indigo-500 to-slate-500 sticky top-0 z-10">
+        <div className="min-h-lvh pt-5 bg-gradient-to-tr from-zinc-200 via-indigo-500 to-slate-500 sticky top-0 z-10">
           <TodoList />
         </div>
       </QueryClientProvider>
