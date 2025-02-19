018cbf5e78b83c7943d3ef79a06d797f31ee8c2b
majdya <majd.ya.1995@gmail.com>
2025-02-19 22:48:12 +0000

    update typing

--- client/src/types/todo.ts
+++ client/src/types/todo.ts
@@ -5,7 +5,6 @@ export interface Todo {
 }
 
 export interface TodoCardProps extends Todo {
-  // onClick: (id: string) => void;
-  // onUpdate: (id: string, newTitle: string) => void;
-  // onRemove: (id: string) => void;
+  onUpdate: (todo: Todo) => void;
+  onRemove: (id: string) => void;
 }
