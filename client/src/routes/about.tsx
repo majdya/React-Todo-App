57c3393b7fe364f9272b0bac642d780a8f84da0f
majdya <majd.ya.1995@gmail.com>
2025-02-20 11:06:42 +0000

    update about page

--- client/src/routes/about.tsx
+++ client/src/routes/about.tsx
@@ -5,5 +5,39 @@ export const Route = createFileRoute("/about")({
 });
 
 function RouteComponent() {
-  return <div>Hello "/about"!</div>;
+  return (
+    <div className="p-4 bg-gray-100 rounded-xl shadow-md max-w-md mx-auto">
+      <h3 className="text-xl font-semibold text-gray-800 mb-2">
+        Todo app developed by <span className="text-blue-600">Majd Yaqub</span>
+      </h3>
+      <p className="text-gray-600">Using:</p>
+      <ul className="list-disc list-inside text-gray-700 mt-1">
+        <li>
+          <span className="font-medium">Vite</span>
+        </li>
+        <li>
+          <span className="font-medium">pnpm</span>
+        </li>
+        <li>
+          <span className="font-medium">TypeScript</span>
+        </li>
+        <li>
+          <span className="font-medium">React</span>
+        </li>
+        <li>
+          <span className="font-medium">Tailwind</span>
+        </li>
+        <li>
+          <span className="font-medium">Shad/cn</span>
+        </li>
+
+        <li>
+          <span className="font-medium">TanStack</span>
+        </li>
+        <li>
+          <span className="font-medium">Supabase</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
